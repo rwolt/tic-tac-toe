445d7a515b8bbb066db70b907e16105164719bf9
Raymond Wolt <rjwolt@gmail.com>
2021-01-02 17:19:46 -0600

    Add HTML for board

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-const gameBoard = (() =>{
-    let board = ['X', 'O'];
+const game = (() => {
+    let board = ['','','','','','','','',''];
+    let playerOne = {};
+    let playerTwo = {};
 
-    return{board};
+    let startGame = () => {
+        let names = document.querySelectorAll('.name');
+        game.playerOne = player(names[0].value, names[0].dataset.mark);
+        game.playerTwo = player(names[1].value, names[1].dataset.mark);
+        game.playerOne.isTurn = true;
+    }
+    return {board, startGame, playerOne, playerTwo};
 })();
 
-const displayController = (() =>{
-    let board = document.getElementById('board');
-    board.textContent = gameBoard.board;
+const display = (() =>{
+    let boardEl = document.getElementById('board');
+    let displayEl = document.getElementById('feedback');
+
+    let message = '';
+    let update = () => {
+        boardEl.textContent = game.board;
+        displayEl.textContent = display.message;
+    }
+    return {update, message};
 })();
 
-const player = (name) => {
-    
-    return{name};
+const player = (name, mark) => {
+    let isTurn = false;
+    let isWinner = false;
+    const placeMark = (index) => {
+        if (game.board[index] == ''){
+            game.board[index] = mark;
+            display.message = `${name} placed an ${mark} at index ${index}`;
+            display.update();
+        } else {
+            display.message = 'Location is already marked';
+            display.update();
+        }
+    }
+    return{name, placeMark, isTurn, isWinner};
 };
+
+
+let startBtn = document.querySelector('#start');
+startBtn.addEventListener('click', game.startGame);
