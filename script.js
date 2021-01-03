bb97ec0828e9fe8c2b5e36b03feb5fa5952a62fa
Raymond Wolt <rjwolt@gmail.com>
2021-01-03 10:46:25 -0600

    Add function to switch turns between players

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
 const game = (() => {
-    let board = ['','','','','','','','',''];
     let playerOne = {};
     let playerTwo = {};
-
-    let startGame = () => {
+    let turn =[];
+    let currentPlayer;
+    let makePlayers = () => {
         let names = document.querySelectorAll('.name');
         game.playerOne = player(names[0].value, names[0].dataset.mark);
         game.playerTwo = player(names[1].value, names[1].dataset.mark);
-        game.playerOne.isTurn = true;
-    }
-    return {board, startGame, playerOne, playerTwo};
+        game.turn = [game.playerOne, game.playerTwo];
+        game.currentPlayer = game.turn[0];
+        } 
+    let switchTurn = () => {
+        switch (game.currentPlayer == game.turn[0]) {
+            case true:
+                game.currentPlayer = game.turn[1];
+                break;
+            case false:
+                game.currentPlayer = game.turn[0];
+                break;
+        }
+        }
+    
+    return {makePlayers, switchTurn, currentPlayer, playerOne, playerTwo};
 })();
 
-const display = (() =>{
-    let boardEl = document.getElementById('board');
-    let displayEl = document.getElementById('feedback');
-
-    let message = '';
-    let update = () => {
-        boardEl.textContent = game.board;
-        displayEl.textContent = display.message;
+const gameBoard = (() => {
+    let board = [];
+    let squares = document.querySelectorAll('.square');
+    let displayEl = document.querySelector('.feedback');
+   
+    function updateMessage(message) {
+        displayEl.textContent = message;
     }
-    return {update, message};
+    function updateBoard() {
+        for (let square of squares) {
+            square.textContent = board[square.id].value;
+        }
+    }
+    return {board, updateBoard, updateMessage};  
 })();
+
 
 const player = (name, mark) => {
-    let isTurn = false;
     let isWinner = false;
     const placeMark = (index) => {
-        if (game.board[index] == ''){
-            game.board[index] = mark;
+        if (gameBoard.board[index] == ''){
+            gameBoard.board[index] = mark;
             display.message = `${name} placed an ${mark} at index ${index}`;
             display.update();
         } else {
@@ -37,9 +53,21 @@ const player = (name, mark) => {
             display.update();
         }
     }
-    return{name, placeMark, isTurn, isWinner};
+
+    // const markSquare = (e) => {
+    //     let currentPlayer = (game.playerOne.isTurn) ? game.playerOne : game.playerTwo;
+    //     currentPlayer.placeMark(e.target.id);
+    //     e.target.textContent = game.currentPlayer.mark;
+    //     }    
+
+    return{name, placeMark, isWinner};
+
 };
 
 
+
+
+
+
 let startBtn = document.querySelector('#start');
-startBtn.addEventListener('click', game.startGame);
+startBtn.addEventListener('click', game.makePlayers);
