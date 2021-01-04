d24c61bec8bdb58b8e0565eedc29b6f0267fb692
Raymond Wolt <rjwolt@gmail.com>
2021-01-03 18:41:44 -0600

    Add comments to describe functionality

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const game = (() => {
     let playerTwo = {};
     let turn =[];
     let currentPlayer;
+    //Populate the two player objects using the factory function
+    //Player 1 gets to go first
     let makePlayers = () => {
         let names = document.querySelectorAll('.name');
         game.playerOne = player(names[0].value, names[0].dataset.mark);
@@ -10,6 +12,7 @@ const game = (() => {
         game.turn = [game.playerOne, game.playerTwo];
         game.currentPlayer = game.turn[0];
     } 
+    //Toggle between the two indexes of the turn array to change whose turn it is
     let switchTurn = () => {
         switch (game.currentPlayer == game.turn[0]) {
             case true:
@@ -33,6 +36,8 @@ const gameBoard = (() => {
     function updateMessage(message) {
         displayEl.textContent = message;
     }
+    //Store the value of the board in an array
+    //Update the targeted DOM element with the contents of the matching array index
     function updateBoard(e) {
             game.currentPlayer.placeMark(e.target.id);
             e.target.textContent = board[e.target.id];
@@ -45,7 +50,9 @@ const gameBoard = (() => {
 })();
 
 const player = (name, mark) => {
+    //Store winning combinations of indexes (3 in a row) in an array
     const winningCombos = [[0,1,2], [0,3,6], [6,7,8], [2,5,8], [0,4,8], [6,4,2], [3,4,5], [1,4,7]];
+    //Function to check the board against a single winning combination
     const checkCombo = (combo, mark) => {
         for(let i = 0; i < 3; i++) {
             let currentCheck = combo[i];
@@ -56,7 +63,8 @@ const player = (name, mark) => {
             }
         }
         return true;
-    }    
+    }
+    //Check the board against all winning combinations  
     const checkWinner = (combosArray, mark) => {
         for(let combo of combosArray) {
             if(checkCombo(combo, mark) == true) {
@@ -67,6 +75,9 @@ const player = (name, mark) => {
         }
             return false;
     }   
+    //Mark the index of the board with the players mark if there already isn't a mark
+    //Check if the player has one at the end of each turn
+    //Return a message if there is already a mark at that index
     const placeMark = (index) => {
         if (gameBoard.board[index] == undefined){
             gameBoard.board[index] = mark;
